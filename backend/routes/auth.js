eea2461559f61f6850135386f07393cc08b0dce3
Daniel Young <dyou6376@gmail.com>
2020-09-10 15:56:11 -0400

    Added validation and password hashing to registratoin

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const {registerValidation, loginValidation} = require('../validation');
 
-router.post('/register', (req,res) => {
-    res.send('register')
+router.post('/register', async (req,res) => {
+    
+    //validate user data
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    //Check if user exists
+    const emailExists = await User.findOne({email: req.body.email});
+    if (emailExists) return res.status(400).send('Email already exists');
+
+    //Hash the pw
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+dotenv.config();
 
 //routes
 const authRoute = require('./routes/auth');
 
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    () => console.log('connected to db')
+);
 
 //middlewares
+app.use(express.json());
 app.use('/api/user', authRoute);
+
 
 
 
